f7d95aa8da9ed6156510c44f3799fc238923a14a
Eviber <youv.didj@gmail.com>
2020-09-29 12:50:35 +0200

    add basic snake skeleton

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
 Game.init = function () {
+	Snake.init();
 }
 
 Game.update = function (delta) {
+	//Snake.turn();
+	Snake.update(delta);
 };
 
 Game.render = function () {
+	Snake.render();
 };
--- snake.js
+++ snake.js
@@ -1,5 +1,13 @@
-var Snake = {
-    pos: (0, 0),
-    length: 6,
-    direction: "up",
+var Snake = {};
+
+Snake.init = function() {
+    this.pos = (0, 0);
+    this.length = 20;
+    this.direction = "up";
 };
+
+Snake.render = function() {};
+
+Snake.update = function(delta) {};
+
+Snake.turn = function(dir) {};
